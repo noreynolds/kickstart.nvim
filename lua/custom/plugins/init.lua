079f5f264138bf87e8aeabc02f4663c184035dc1
Nicholas R. <noreynolds2@gmail.com>
2026-04-02 21:00:11 -0400

    +cline plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,20 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  {
+    'carlos-algms/agentic.nvim',
+    opts = {
+      provider = 'cline-acp',
+      acp_providers = {
+        ['cline-acp'] = {
+          command = 'cline',
+          args = { '--acp' },
+        },
+      },
+    },
+    keys = {
+      { '<C-\\>', function() require('agentic').toggle() end, mode = { 'n', 'v', 'i' }, desc = 'Toggle Cline Chat' },
+    },
+  },
+}
